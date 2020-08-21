f9afef0fed5ab03eba04fe3eb0ac0e205913a45e
zomervinicius <vinicius.zomer123@gmail.com>
2020-08-21 15:37:51 -0300

    fix: remove lineheight from typography

--- src/theme.tsx
+++ src/theme.tsx
@@ -4,40 +4,33 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   typography: {
     subtitle1: {
-      lineHeight: 12,
       fontSize: 12,
       fontWeight: 400,
     },
     subtitle2: {
-      lineHeight: 12,
       fontSize: 14,
       fontWeight: 400,
     },
     body1: {
-      lineHeight: 20,
       fontSize: 12,
       fontWeight: 500,
     },
     body2: {
       fontSize: 14,
-      lineHeight: 20,
       fontWeight: 500,
     },
     h6: {
-      lineHeight: 25,
       fontSize: 14,
       letterSpacing: 0.1,
       fontWeight: 700,
     },
     h5: {
-      lineHeight: 30,
       fontSize: 16,
       fontWeight: 700,
     },
     caption: {
       fontFamily: 'Montserrat',
       fontSize: 32,
-      lineHeight: 40,
       letterSpacing: -1.5,
     },
   },
